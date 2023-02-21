c31bc36656807a0a470dbd357be8f49f786e70df
jakobsperry <sperryjakob@gmail.com>
2023-02-21 08:36:47 -0500

    added serial reading

--- src/Display.h
+++ src/Display.h
@@ -9,15 +9,20 @@ class Display{
 
     void init();
     void sendPoint(int Ix, int Iy);
+    void dispDistAn(int dist);
+    void dispDistPw(int dist);
+    void dispDistRX(String dist);
+
     void update();
     void clear();
+    void format();
 
     private:
 
     #define SCREEN_WIDTH 128 // OLED display width, in pixels
     #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
-    #define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+    #define OLED_RESET     17 // Reset pin # (or -1 if sharing Arduino reset pin)
     #define SCREEN_ADDRESS 0x3D ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
    
 };
